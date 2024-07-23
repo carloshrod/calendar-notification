0d40d56a7dac0785f69ea2cb8ff2cd6940ed89dc
chrod <chrod.dev@gmail.com>
2024-07-22 20:32:56 -0500

    Handle days 29, 30 and 31

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -9,21 +9,48 @@ admin.initializeApp({
 });
 
 exports.executeSendingNotifications = functions.pubsub
-  .schedule("30 16 * * *") // minuto(0-59) | hora(0-23) | día_del_mes(0-31) | mes(1-12) | día_de_la_semana(0-6 / Dom = 0)
-  .timeZone("America/Bogota") // America/Mexico_City
+  .schedule("0 12 * * *") // minuto(0-59) | hora(0-23) | día_del_mes(0-31) | mes(1-12) | día_de_la_semana(0-6 / Dom = 0)
+  .timeZone("America/Mexico_City") // America/Mexico_City
   .onRun(async (context) => {
     console.log("Ejecutando envío de notificaciones!");
 
     const now = new Date();
-    const day = now.getDate();
+    const day = now.getDate(); // Día actual
+    const lastDayOfMonth = new Date(
+      now.getFullYear(),
+      now.getMonth() + 1,
+      0
+    ).getDate();
+
+    console.log({ lastDayOfMonth });
+    console.log({ day });
 
     const notificationsRef = admin.firestore().collection("notifications");
-    const snapshot = await notificationsRef.where("notifDay", "==", day).get();
 
-    if (snapshot.empty)
+    // Obtener notificaciones para el día actual
+    let snapshot = await notificationsRef.where("notifDay", "==", day).get();
+    let docs = snapshot.docs;
+
+    // Si hoy es el último día del mes, también obtenemos notificaciones para los posibles días faltantes 29, 30, y 31
+    if (day === lastDayOfMonth) {
+      // const additionalDays = [29, 30, 31];
+      const additionalDays = [23, 24, 25];
+      // const additionalDays = [21, 22, 23];
+      for (const extraDay of additionalDays) {
+        // Se obtienen las notificaciones adicionales siempre y cuando el día actual sea menor el día faltante (extraDay). Por ejemplo si el día actual es 30, se omite el día 29 porque ya pasó, se omite el día 30 porque es el día actual y ya se obtuvieron las notificaciones para este (línea 31) y solo se obtienen las notificaciones adicionales para el día 31 (si estas existen) que es el único posible día faltante
+        if (day < extraDay) {
+          const extraSnapshot = await notificationsRef
+            .where("notifDay", "==", extraDay)
+            .get();
+          docs = docs.concat(extraSnapshot.docs);
+        }
+      }
+    }
+
+    if (docs.length === 0)
       return console.log("No hay notificaciones para el día de hoy!");
 
-    snapshot.forEach((doc) => {
+    docs.forEach((doc) => {
       const data = doc.data();
       const email = data.email;
       const subject = "Notificación Diaria";
